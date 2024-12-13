22b3231cdc7f6615a7f7a83b2411ad39f0800b22
keyboardmunji <wjdwngus12155@naver.com>
2024-12-13 19:17:39 +0900

    Update 소스.c

--- 소스.c
+++ 소스.c
@@ -2,11 +2,13 @@
 
 int main(void)
 {
-	printf("홍길동\n홍 길 동\n홍  길  동\n"); // Q: 2-1
-	
-	printf("name : jjh\n"); // Q:2-2
-	printf("address : none\n");
-	printf("phone number: none\n"); 
+	printf("제 이름은 홍길동 입니다.\n"); // Q:2-2-1
+	printf("내 나이는 %d살이고요.\n",20);
+	printf("제가 사는 곳의 번지수는 %d-%d입니다.\n",123,456);
+
+	printf("%d X %d = %d\n", 4, 5, 20); // Q:2-2-2
+	printf("%d X %d = %d\n", 7, 9, 63);
+
 	return 0;
 
 }
