bf6b83503f8836ab07a96658e9b4a2b47e3e1fff
keyboardmunji <wjdwngus12155@naver.com>
2024-12-13 17:15:18 +0900

    Add files via upload
    Book's question to study C language

---
+++ 소스.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("홍길동\n홍 길 동\n홍  길  동\n"); // Q: 2-1
+	
+	printf("name : jjh\n"); // Q:2-2
+	printf("address : none\n");
+	printf("phone number: none\n"); 
+	return 0;
+
+}
