20598614ab0fa75dcf8e5ddbcb68588bc9ac4bd5
keyboardmunji <wjdwngus12155@naver.com>
2024-12-16 23:11:03 +0900

    Chapter 5 Question
    Book's question to learn C language

--- 소스.c
+++ 소스.c
@@ -3,14 +3,29 @@
 
 int main(void)
 {
-	//Q:4-4-1 입력받은 정수값을 비트연산자를 이용하여 부호를 바꾸어 출력하기.
-	int num1;
-	printf("정수 1개를 입력하세요. : ");
-	scanf("%d", &num1);
-	num1 = (~num1) + 1;
-	printf("%d\n", num1);
-	//Q:4-4-2 3*8/4를 비트 연산자를 통해 구현 해보자.
-	printf("3*8/4의 계산결과 : %d", (3 << 3) >> 2);
+	// Q: 5 - 1 - 1 좌하단 x,y값과 우상단 x,y값을 받은 후 직사각형 넓이 계산
+	int x1, x2, y1, y2, result;
+	char b;
+	double num1, num2;
+	int a;
+	printf("좌하단 x,y좌표 입력 : ");
+	scanf("%d %d", &x1, &y1);
+	printf("우상단 x,y좌표 입력 : ");
+	scanf("%d %d", &x2, &y2);
+	result = (x2 - x1) * (y2 - y1);
+	printf("직사각형 넓이는 %d 입니다.\n", result);
+	// Q : 5-1-2 : 실수 2개받고 사칙연산 진행
+	printf("두 개의 실수 입력하세요 : ");
+	scanf("%lf %lf", &num1, &num2);
+	printf("덧셈 : %f 뺄셈 : %f 곱셉 : %f 나눗셈 : %f\n", num1 + num2, num1 - num2, num1 * num2, num1 / num2);
+	//Q : 5-1-4 : 사용자가 아스키 코드값 입력시 그에 상응하는 문자 출력*/
+	printf("아스키 코드 정수 입력 : ");
+	scanf("%d", &a);
+	printf("상응하는 문자 : %c\n", a);
+	// Q : 5-1-5 : 사용자가 문자 입력시 그에 맞는 아스키 코드 출력
+	printf("문자 한개 입력 : ");
+	scanf(" %c", &b);
+	printf("%d", b);
 	return 0;
 
 }
