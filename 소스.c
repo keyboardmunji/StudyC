503e06ec66760f9d7c27fe0d9f40f18595dc5b8b
keyboardmunji <wjdwngus12155@naver.com>
2024-12-22 23:16:24 +0900

    Chapter 17 Question
    Book's Question to learn C language

--- 소스.c
+++ 소스.c
@@ -1,69 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+void MaxAndMin(int[], int, int**, int**);
+
 int main()
 {
-	//Q : 16-1-1 : 구구단 2,3,4단을 2차원 배열에 저장 후 출력
-	int a[3][9];
-	int num[2][4] = { {1,2,3,4,},{5,6,7,8} };
-	int num1[4][2];
-	int num2[5][5];
-	int i, j, sum1, sum2;
-	for (i = 2; i < 5; i++)
-		for (j = 1; j <= 9; j++)
-			a[i - 2][j - 1] = i * j;
-	printf("저장 한 구구단 출력 \n");
-	for (i = 0; i < 3; i++)
-		for (j = 0; j < 9; j++)
-			printf("%d * %d = %d\n", i + 2, j + 1, a[i][j]);
-	//Q ; 16-1-2 : 배열 돌리기?
-	printf("\n");
-	for (i = 0; i < 2; i++)
-	{
-		for (j = 0; j < 4; j++)
-			printf("%d", num[i][j]);
-		printf("\n");
-	}
-	printf("\n");
-	for (i = 0; i < 2; i++)
-		for (j = 0; j < 4; j++)
-			num1[j][i] = num[i][j];
-	for (i = 0; i < 4; i++)
-	{
-		for (j = 0; j < 2; j++)
-			printf("%d", num1[i][j]);
-		printf("\n");
-	}
-	printf("\n");
-	//Q : 16-1-3 : 4명의 사람 4개의 과목별 총점 계산 후 저장
-	for (i = 0; i < 4; i++)
-	{
-		printf("%d번째 사람 점수 입력 : ", i + 1);
-		scanf("%d %d %d %d", &num2[i][0], &num2[i][1], &num2[i][2], &num2[i][3]);
-	}
-	for (i = 0; i < 4; i++)
-	{
-		sum1 = sum2 = 0;
-		for (j = 0; j < 4; j++)
-		{
-			sum1 += num2[i][j];
-			sum2 += num2[j][i];
-		}
-		num2[i][4] = sum1;
-		num2[4][i] = sum2;
-	}
-	for (i = 0, sum1 = 0; i < 4; i++)
-		sum1 += num2[4][i];
-	num2[4][4] = sum1;
-	printf("\n 점수 계산 완료 \n");
+	//Q : 17-1-1 : maxptr에는 가장 큰 값의 주소, minptr에는 가장 작은 값의 주소를 담는 그런 함수만들기
+	int* maxptr;
+	int* minptr;
+	int arr[5], i;
+	printf("5개의 정수 입력 \n");
 	for (i = 0; i < 5; i++)
-	{
-		for (j = 0; j < 5; j++)
-			printf("%3d", num2[i][j]);
-		printf("\n");
-	}
+		scanf("%d", &arr[i]);
+	MaxAndMin(arr, sizeof(arr) / sizeof(int), &maxptr, &minptr);
+	printf("최댓값 : %d, 최솟값 : %d\n", *maxptr, *minptr);
 
 	return 0;
 }
 
+void MaxAndMin(int arr[],int len, int** max, int** min)
+{
+	int i;
+	*max = *min = &arr[0];
+	for (i = 1; i < len; i++)
+	{
+		if (arr[i] > **max)
+			*max = &arr[i];
+		else if (arr[i] < **min)
+			*min = &arr[i];
+	}
+}
 
